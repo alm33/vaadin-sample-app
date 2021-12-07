d9cfe67aa9e961af66ff2f0b70d7acf6cfa340ac
Alain Murat <alain.murat.pro@gmail.com>
2021-12-07 15:40:16 +0000

    Init Spring boot project

---
+++ src/test/java/com/alm33/vaadin/sample/app/SampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alm33.vaadin.sample.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
